9f7768ea4be73d7fb9eded6f418a954d79050937
SergiRius2003 <115240797+SergiRius2003@users.noreply.github.com>
2024-03-29 19:15:16 +0100

    serie_model

--- series/models.py
+++ series/models.py
@@ -6,3 +6,10 @@ from productions.models import production
 # Create your models here.
 class serie(models.Model):
     idProduction = models.ForeignKey(production,on_delete=models.CASCADE)
+    serieName = models.CharField(max_length=100,default="noSerie")
+    overview = models.TextField(default="No overview")
+    number_of_episodes = models.IntegerField(default=0)
+    number_of_seasons = models.IntegerField(default=0)
+    season_number = models.IntegerField(default=0)
+    in_production = models.BooleanField(default=False)
+    original_language = models.CharField(max_length=100,default="")
