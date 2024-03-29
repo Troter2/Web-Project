30b40eb6b09170ba3542137f4846cc16b0979be6
Cinta Miralles Jarque <141917240+atnic15@users.noreply.github.com>
2024-03-29 16:46:53 +0100

    removd idea files

--- movies/models.py
+++ movies/models.py
@@ -6,3 +6,4 @@ from productions.models import production
 # Create your models here.
 class movie(models.Model):
     idProduction = models.ForeignKey(production, on_delete=models.CASCADE)
+    nom = models."tipo de camp"
