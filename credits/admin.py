066ec0410426e4cd52677bd0e2e8e4ced76cd8f4
Cinta Miralles Jarque <141917240+atnic15@users.noreply.github.com>
2024-03-29 18:49:50 +0100

    Productor

--- credits/admin.py
+++ credits/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from credits.models import credit
+from credits.models import credit, productor
+
 # Register your models here.
 admin.site.register(credit)
+admin.site.register(productor)
