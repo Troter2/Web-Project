230a175fe2d245f2bbdde0d1a0e27548ea6444df
SergiRius2003 <115240797+SergiRius2003@users.noreply.github.com>
2024-04-02 19:46:36 +0200

    Update models.py

--- series/models.py
+++ series/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class serie(models.Model):
@@ -8,7 +7,7 @@ class serie(models.Model):
     overview = models.TextField(default="No overview")
     number_of_episodes = models.IntegerField(default=0)
     number_of_seasons = models.IntegerField(default=0)
-    season_number = models.IntegerField(default=0)
+    # season_number = models.IntegerField(default=0)
     in_production = models.BooleanField(default=False)
     original_language = models.CharField(max_length=100, default="")
 
@@ -23,6 +22,11 @@ class seasonDetail(models.Model):
     season_number = models.IntegerField()
     vote_average = models.FloatField(blank=True, null=True)
 
+    def clean(self):
+        if self.season_number >= self.serie.number_of_seasons:
+            raise ValidationError(
+                "El número de temporada debe ser menor que el número total de temporadas de la serie.")
+
 
 class episodeDetail(models.Model):
     air_date = models.DateField()
@@ -35,3 +39,13 @@ class episodeDetail(models.Model):
     season = models.ForeignKey(seasonDetail, on_delete=models.CASCADE)
     still_path = models.CharField(max_length=255, default="")
     vote_average = models.FloatField(blank=True, null=True)
+
+    def clean(self):
+        if self.episode_number >= self.season.episodes:
+            raise ValidationError(
+                "El número de espisodio debe ser menor que el número total de espisodios de la temporada.")
+
+    def clean(self):
+        if self.air_date <= self.season.air_date:
+            raise ValidationError(
+                "Un espodi tiene que estar creado antes o igual que su temporada correspondiente.")
