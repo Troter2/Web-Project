3c9a23f9005a8d2249a00ddad1993de97292f965
Sergi Vila <47522460+Troter2@users.noreply.github.com>
2024-04-02 19:46:57 +0200

    Modify login

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -7,3 +8,6 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+def admin_redirect(request):
+    return redirect('/admin')
