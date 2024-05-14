6817fb15b125ad341c7aa7d14cf087ed11eeb3cb
Abdellah Lamrabat <udlunieps@gmail.com>
2024-05-14 11:56:29 +0200

    feat: adapt dev branch to this branch

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,6 +6,7 @@ from reviews.views import movie_details
 
 
 urlpatterns = [
+    path('new-review/<int:movieId>/', movie_details, name='new_review'),
     path('list-movies/', list_movies, name='list_movies'),
     path('movie-detail/<int:movieId>/', movie_details, name='movie_details'),
     ]
