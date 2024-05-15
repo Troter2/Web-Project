44e82cb6adfa54b496004e3162d4cebe06ca9b43
Abdellah Lamrabat <udlunieps@gmail.com>
2024-05-15 20:15:08 +0200

    feat: issue implemented

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from movies.views import list_movies
-from reviews.views import movie_details, list_my_reviews, delete_review
+from reviews.views import movie_details, list_my_reviews, delete_review, edit_review
 
 
 urlpatterns = [
+    path('edit-review/<int:reviewId>/', edit_review, name='edit_review'),
     path('my-reviews/', list_my_reviews, name='my_reviews'),
     path('new-review/<int:movieId>/', movie_details, name='new_review'),
     path('list-movies/', list_movies, name='list_movies'),
     path('movie-detail/<int:movieId>/', movie_details, name='movie_details'),
     path('delete_review/<int:reviewId>/', delete_review, name='delete_review'),
+
     ]
