f5e442bdc3f864a8cdc6d94327271afb7e42a3b7
Miquel Elena Pérez <75449266+Miquel538@users.noreply.github.com>
2024-05-17 18:48:50 +0200

    Update test_authentication.py

--- PlaywrightTests/test_authentication.py
+++ PlaywrightTests/test_authentication.py
@@ -17,20 +17,21 @@ def setup_teardown():
 def test_signup(setup_teardown: Page):
     page = setup_teardown
     page.get_by_role("link", name="Sing up").click()
-    page.get_by_placeholder("Username").click()
-    page.get_by_placeholder("Username").fill("aurelio")
-    page.get_by_placeholder("Email").click()
-    page.get_by_placeholder("Email").fill("aurelio@alumnes.udl.cat")
-    page.get_by_label("Pasword").click()
-    page.get_by_label("Pasword").fill("aurelio123")
-    page.get_by_role("textbox", name="name@example.com").click()
-    page.get_by_role("textbox", name="name@example.com").fill("aurelio123")
     expect(page.get_by_role("img", name="login form")).to_be_visible()
     expect(page.locator("span")).to_contain_text("Sign up")
+    expect(page.get_by_role("paragraph")).to_contain_text("Already have an account? Log in here")
+    page.get_by_placeholder("Username").fill("manolo")
+    page.get_by_placeholder("Email").click()
+    page.get_by_placeholder("Email").fill("manolo@alumnes.udl.cat")
+    page.get_by_label("Pasword").click()
+    page.get_by_label("Pasword").fill("manolo123")
+    page.get_by_role("textbox", name="name@example.com").click()
+    page.get_by_role("textbox", name="name@example.com").fill("manolo123")
     page.get_by_role("button", name="Register").click()
 
 
 def test_signup_with_account(setup_teardown: Page):
+    page = setup_teardown
     page.get_by_role("link", name="Sing up").click()
     expect(page.get_by_role("img", name="login form")).to_be_visible()
     expect(page.locator("span")).to_contain_text("Sign up")
@@ -50,3 +51,21 @@ def test_signup_with_account(setup_teardown: Page):
 
 
 def login_test(setup_teardown: Page):
+    page = setup_teardown
+    page.get_by_role("link", name="Log In").click()
+    expect(page.get_by_role("img", name="login form")).to_be_visible()
+    expect(page.locator("span")).to_contain_text("Sign in")
+    page.get_by_label("Usuario").click()
+    page.get_by_label("Usuario").fill("aurelio")
+    page.get_by_label("Contraseña").click()
+    page.get_by_label("Contraseña").fill("aurelio123")
+    page.get_by_role("button", name="Login").click()
+
+
+def login_test_register(setup_teardown: Page):
+    page = setup_teardown
+    page.get_by_role("link", name="Log In").click()
+    expect(page.locator("span")).to_contain_text("Sign in")
+    expect(page.get_by_role("img", name="login form")).to_be_visible()
+    expect(page.get_by_role("paragraph")).to_contain_text("Don't have an account? Register here")
+    page.get_by_role("link", name="Register here").click()
